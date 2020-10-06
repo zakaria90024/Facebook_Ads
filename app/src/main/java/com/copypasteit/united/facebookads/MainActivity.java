c00137bdc40fa89ed8cadfbdd9dd1ccfeec9f460
MDZAKARIA <zakaria112203@gmail.com>
2020-10-06 17:48:46 +0600

    Initial commit

---
+++ app/src/main/java/com/copypasteit/united/facebookads/MainActivity.java
@@ -0,0 +1,196 @@
+package com.copypasteit.united.facebookads;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import com.facebook.ads.AudienceNetworkAds;
+//step 2 banner
+import com.facebook.ads.*;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView adView;
+
+
+    //setp 1 for intertesial
+    private final String TAG = MainActivity.class.getSimpleName();
+    private InterstitialAd interstitialAd;
+
+
+
+    //step 1 for video
+    //private final String TAG = MainActivity.class.getSimpleName();
+    private RewardedVideoAd rewardedVideoAd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //step 1 banner
+        AudienceNetworkAds.initialize(this);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //step 3 is layout
+        //step 4 --------------------
+        //for faceboo ads
+
+        adView = new AdView(this, "IMG_16_9_APP_INSTALL#2811380842414575_2811462872406372", AdSize.BANNER_HEIGHT_50);
+        // Find the Ad Container
+        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
+        // Add the ad view to your activity layout
+        adContainer.addView(adView);
+        // Request an ad
+        adView.loadAd();
+
+
+
+        //step 02 for intertial
+
+        interstitialAd = new InterstitialAd(this, "IMG_16_9_APP_INSTALL#2811380842414575_2811476129071713");
+
+        // Create listeners for the Interstitial Ad
+        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(Ad ad) {
+                // Interstitial ad displayed callback
+                Log.e(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(Ad ad) {
+                // Interstitial dismissed callback
+                Log.e(TAG, "Interstitial ad dismissed.");
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // Show the ad
+                interstitialAd.show();
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+                Log.d(TAG, "Interstitial ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+                Log.d(TAG, "Interstitial ad impression logged!");
+            }
+        };
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd(
+                interstitialAd.buildLoadAdConfig()
+                        .withAdListener(interstitialAdListener)
+                        .build());
+
+
+
+
+
+
+
+
+        ///step -2 for reward video
+
+        rewardedVideoAd = new RewardedVideoAd(this, "IMG_16_9_APP_INSTALL#2811380842414575_2811482245737768");
+        RewardedVideoAdListener rewardedVideoAdListener = new RewardedVideoAdListener() {
+            @Override
+            public void onError(Ad ad, AdError error) {
+                // Rewarded video ad failed to load
+                Log.e(TAG, "Rewarded video ad failed to load: " + error.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Rewarded video ad is loaded and ready to be displayed
+                Log.d(TAG, "Rewarded video ad is loaded and ready to be displayed!");
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Rewarded video ad clicked
+                Log.d(TAG, "Rewarded video ad clicked!");
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Rewarded Video ad impression - the event will fire when the
+                // video starts playing
+                Log.d(TAG, "Rewarded video ad impression logged!");
+            }
+
+            @Override
+            public void onRewardedVideoCompleted() {
+                // Rewarded Video View Complete - the video has been played to the end.
+                // You can use this event to initialize your reward
+                Log.d(TAG, "Rewarded video completed!");
+
+                // Call method to give reward
+                // giveReward();
+            }
+
+            @Override
+            public void onRewardedVideoClosed() {
+                // The Rewarded Video ad was closed - this can occur during the video
+                // by closing the app, or closing the end card.
+                Log.d(TAG, "Rewarded video ad closed!");
+            }
+        };
+        rewardedVideoAd.loadAd(
+                rewardedVideoAd.buildLoadAdConfig()
+                        .withAdListener(rewardedVideoAdListener)
+                        .build());
+
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+
+        if (rewardedVideoAd != null) {
+            rewardedVideoAd.destroy();
+            rewardedVideoAd = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MainActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+        super.onBackPressed();
+    }
+}
